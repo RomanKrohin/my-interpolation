00dc7ae2982723d7c6ae191d5941eb99ca783a63
Roman <romankrohin126@gmail.com>
2024-10-20 21:09:47 +0300

    fix: lagrnage fixx

--- interpolation/src/interpolation/core.clj
+++ interpolation/src/interpolation/core.clj
@@ -1,15 +1,19 @@
 (ns interpolation.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-;; 1. Функция для парсинга входной строки
+(def cli-options
+  [["-s" "--step STEP" "Шаг интерполяции" :parse-fn #(Double/parseDouble %)]
+   ["-h" "--help"]])
+
 (defn parse-line [line]
   (let [[x y] (str/split line #"\s+")]
     [(Double/parseDouble x) (Double/parseDouble y)]))
 
-;; 2. Линейная интерполяция между двумя точками
-(defn linear-interpolation [p1 p2 step]
-  (let [[x1 y1] p1
+(defn linear-interpolation [points step]
+  (let [[p1 p2] points
+        [x1 y1] p1
         [x2 y2] p2
         x-values (range x1 (+ x2 step) step)]
     (map (fn [x]
@@ -17,7 +21,6 @@
              [x (+ y1 (* t (- y2 y1)))]))
          x-values)))
 
-;; 3. Интерполяция Лагранжа для множества точек
 (defn lagrange-polynomial [points x]
   (let [n (count points)]
     (reduce
@@ -34,41 +37,49 @@
      0
      (range n))))
 
-;; 4. Вычисление Лагранжевой интерполяции на множестве точек
 (defn lagrange-interpolation [points step start-x end-x]
   (let [x-values (range start-x (+ end-x step) step)]
-    ;; Проверяем корректность диапазона
-    (if (empty? x-values)
-      (throw (IllegalArgumentException. "Диапазон x-значений пуст! Убедитесь, что start-x и end-x различаются."))
-      (map (fn [x] [x (lagrange-polynomial points x)]) x-values))))
+    (map (fn [x] [x (lagrange-polynomial points x)]) x-values)))
 
-;; 5. Чтение и обработка входных данных
-(defn process-input []
-  (let [data (atom [])]
+(defn format-output [x-values y-values]
+  (let [x-str (str/join "\t" (map #(format "%.2f" %) x-values))
+        y-str (str/join "\t" (map #(format "%.2f" %) y-values))]
+    (str x-str "\n" y-str)))
+
+(defn process-input [step]
+  (let [window (atom [])]
     (while true
       (let [line (read-line)]
         (when (not (empty? line))
           (let [point (parse-line line)]
-            (swap! data conj point)
-            (println "Введены точки: " @data)
+            (swap! window conj point)
 
-            ;; Линейная интерполяция при наличии 2 точек
-            (when (>= (count @data) 2)
-              (let [[p1 p2] (take-last 2 @data)]
-                (println "Линейная интерполяция:")
-                (doseq [[x y] (linear-interpolation p1 p2 1)]
-                  (println (format "%.2f\t%.2f" x y)))))
+            ;; Линейная интерполяция: выполняется после каждых 2 точек
+            (when (>= (count @window) 2)
+              (let [window-points (take-last 2 @window)
+                    interp (linear-interpolation window-points step)
+                    x-values (map first interp)
+                    y-values (map second interp)]
+                (println (format "Линейная интерполяция (X от %.3f до %.3f):"
+                                 (first x-values) (last x-values)))
+                (println (format-output x-values y-values))))
 
-            ;; Лагранжевая интерполяция при наличии минимум 3 точек
-            (when (>= (count @data) 3)
-              (println "Лагранжевая интерполяция:")
-              (try
-                (doseq [[x y] (lagrange-interpolation @data 1 (ffirst @data) (ffirst (last @data)))]
-                  (println (format "%.2f\t%.2f" x y)))
-                (catch IllegalArgumentException e
-                  (println (.getMessage e)))))))))))
+            ;; Лагранжевая интерполяция: выполняется после каждых 5 точек
+            (when (>= (count @window) 5)
+              (let [window-points (take-last 5 @window)
+                    start-x (first (map first window-points))
+                    end-x (last (map first window-points))
+                    interp (lagrange-interpolation window-points step start-x end-x)
+                    x-values (map first interp)
+                    y-values (map second interp)]
+                (println (format "Лагранжевская интерполяция (X от %.3f до %.3f):"
+                                 start-x end-x))
+                (println (format-output x-values y-values))))))))))
 
-;; 6. Главная функция программы
 (defn -main [& args]
-  (println "Введите точки в формате: X Y")
-  (process-input))
+  (let [{:keys [options]} (parse-opts args cli-options)]
+    (if (:help options)
+      (println "Usage: lein run --step <step>")
+      (let [step (or (:step options) 1.0)]
+        (println "Введите точки в формате: X Y")
+        (process-input step)))))
